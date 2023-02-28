b4ff6d4e93132bf8f4977f2b537ee6368893f49c
Md Tariq-ul Islam <dev.tariqul.islam@gmail.com>
2023-02-28 13:52:24 +0600

    Update queries.sql file

--- queries.sql
+++ queries.sql
@@ -239,8 +239,11 @@ HAVING COUNT(*) = (
 );
 
 
-EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animal_id = 4;
+EXPLAIN (ANALYZE, BUFFERS) 
+SELECT COUNT(*) FROM visits where animal_id = 4;
 
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN (ANALYZE, BUFFERS) 
+SELECT * FROM visits where vet_id = 2;
 
-EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+EXPLAIN (ANALYZE, BUFFERS) 
+SELECT * FROM owners where email = 'owner_18327@mail.com';
