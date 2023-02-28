89f04bbf1ae63ea9de0780bbe483a0841c5f9a76
tsohleDev <mokhemisitsohle@gmail.com>
2023-02-28 09:29:25 +0200

    Explain Analyze the queries with screenshots

--- queries.sql
+++ queries.sql
@@ -237,3 +237,10 @@ HAVING COUNT(*) = (
     GROUP BY species.name
   ) AS counts
 );
+
+
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animal_id = 4;
+
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
